5a98e00d9e7bf4e13fd11cc376042bd961e27bae
Travis Redman <travis.redman@gmail.com>
2019-02-03 11:21:04 -0800

    [logger] ensure HoneycombLogger does not drop loglevel setting
    We set LogLevel before HoneycombLogger.Start is called. Currently, start creates a new HoneycombLoggerConfig object,
    then fills it with values from the config file. Since the HoneycombLogger section does not include log level, we effectively
    reset the loglevel value to 0 and log everything.

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,1 +1,22 @@
 package logger
+
+import (
+	"testing"
+
+	"github.com/honeycombio/samproxy/config"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHoneycombLoggerRespectsLogLevelAfterStart(t *testing.T) {
+	cfg := &config.MockConfig{GetOtherConfigVal: `{}`}
+	hcLogger := &HoneycombLogger{
+		Config:       cfg,
+		loggerConfig: HoneycombLoggerConfig{level: WarnLevel},
+	}
+
+	assert.Equal(t, WarnLevel, hcLogger.loggerConfig.level)
+	err := hcLogger.Start()
+	assert.Nil(t, err)
+	assert.Equal(t, WarnLevel, hcLogger.loggerConfig.level)
+}
