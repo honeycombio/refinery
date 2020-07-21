e86795150b868aac35ce92bd59a571797d695813
Alaina Valenzuela <asdvalenzuela@gmail.com>
2020-07-21 13:46:20 -0700

    add a test

--- transmit/transmit_test.go
+++ transmit/transmit_test.go
@@ -1,1 +1,27 @@
 package transmit
+
+import (
+	"testing"
+
+	"github.com/honeycombio/samproxy/config"
+	"github.com/honeycombio/samproxy/logger"
+	"github.com/honeycombio/samproxy/metrics"
+
+	libhoney "github.com/honeycombio/libhoney-go"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultTransmissionUpdatesUserAgentAdditionAfterStart(t *testing.T) {
+	transmission := &DefaultTransmission{
+		Config:     &config.MockConfig{},
+		Logger:     &logger.NullLogger{},
+		Metrics:    &metrics.NullMetrics{},
+		LibhClient: &libhoney.Client{},
+		Version:    "test",
+	}
+
+	assert.Equal(t, libhoney.UserAgentAddition, "")
+	err := transmission.Start()
+	assert.Nil(t, err)
+	assert.Equal(t, libhoney.UserAgentAddition, "samproxy/test")
+}
