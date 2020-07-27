c12bf0f5ba4051d37b36d43d22372a89ad13b164
Martin Holman <martin308@gmail.com>
2020-07-27 07:11:14 -0700

    Move this to the internal packages dir

--- cmd/test_redimem/main.go
+++ cmd/test_redimem/main.go
@@ -13,7 +13,7 @@ import (
 	"github.com/garyburd/redigo/redis"
 	"github.com/sirupsen/logrus"
 
-	"github.com/honeycombio/samproxy/config/redimem"
+	"github.com/honeycombio/samproxy/internal/redimem"
 )
 
 func main() {
