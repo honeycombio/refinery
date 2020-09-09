bdf2ded04c5244471d494ede3f721109d270a6e2
Ian Wilkes <ian.wilkes@gmail.com>
2020-09-09 16:06:22 -0700

    basic end-to-end event test

--- app/app_test.go
+++ app/app_test.go
@@ -1,8 +1,11 @@
 package app
 
 import (
-	"io/ioutil"
+	"bytes"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
@@ -22,7 +25,7 @@ import (
 	"github.com/honeycombio/samproxy/transmit"
 )
 
-func newStartedApp(t *testing.T) (*App, inject.Graph) {
+func newStartedApp(t *testing.T, libhoneyOut io.Writer) (*App, inject.Graph) {
 	c := &config.MockConfig{
 		GetSendDelayVal:          0,
 		GetTraceTimeoutVal:       60 * time.Second,
@@ -34,17 +37,18 @@ func newStartedApp(t *testing.T) (*App, inject.Graph) {
 		GetPeerBufferSizeVal:     10000,
 		GetListenAddrVal:         "127.0.0.1:11000",
 		GetPeerListenAddrVal:     "127.0.0.1:11001",
+		GetAPIKeysVal:            []string{"KEY"},
 	}
 
 	peers, err := peer.NewPeers(c)
-	assert.Equal(t, nil, err)
+	assert.NoError(t, err)
 
 	a := App{}
 
 	lgr := &logger.LogrusLogger{
 		Config: c,
 	}
-	lgr.SetLevel("debug")
+	lgr.SetLevel("info")
 	lgr.Start()
 
 	// TODO use real metrics
@@ -57,15 +61,15 @@ func newStartedApp(t *testing.T) (*App, inject.Graph) {
 
 	upstreamClient, err := libhoney.NewClient(libhoney.ClientConfig{
 		Transmission: &transmission.WriterSender{
-			W: ioutil.Discard,
+			W: libhoneyOut,
 		},
 	})
-	assert.Equal(t, nil, err)
+	assert.NoError(t, err)
 
 	peerClient, err := libhoney.NewClient(libhoney.ClientConfig{
 		Transmission: &transmission.DiscardSender{},
 	})
-	assert.Equal(t, nil, err)
+	assert.NoError(t, err)
 
 	var g inject.Graph
 	err = g.Provide(
@@ -82,13 +86,13 @@ func newStartedApp(t *testing.T) (*App, inject.Graph) {
 		&inject.Object{Value: samplerFactory},
 		&inject.Object{Value: &a},
 	)
-	assert.Equal(t, nil, err)
+	assert.NoError(t, err)
 
 	err = g.Populate()
-	assert.Equal(t, nil, err)
+	assert.NoError(t, err)
 
 	err = startstop.Start(g.Objects(), nil)
-	assert.Equal(t, nil, err)
+	assert.NoError(t, err)
 
 	// Racy: wait just a moment for ListenAndServe to start up.
 	time.Sleep(10 * time.Millisecond)
@@ -96,8 +100,38 @@ func newStartedApp(t *testing.T) (*App, inject.Graph) {
 }
 
 func TestAppIntegration(t *testing.T) {
-	_, graph := newStartedApp(t)
+	var out bytes.Buffer
+	_, graph := newStartedApp(t, &out)
 
-	err := startstop.Stop(graph.Objects(), nil)
-	assert.Equal(t, nil, err)
+	// Send a root span, it should be sent in short order.
+	req := httptest.NewRequest(
+		"POST",
+		"http://localhost:11000/1/batch/dataset",
+		strings.NewReader(`[{"data":{"trace.trace_id":"1","foo":"bar"}}]`),
+	)
+	req.Header.Set("X-Honeycomb-Team", "KEY")
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	resp.Body.Close()
+
+	// Wait for span to be sent.
+	deadline := time.After(time.Second)
+	for {
+		if out.Len() > 62 {
+			break
+		}
+		select {
+		case <-deadline:
+			t.Error("timed out waiting for output")
+			return
+		case <-time.After(time.Millisecond):
+		}
+	}
+	assert.Equal(t, `{"data":{"foo":"bar","trace.trace_id":"1"},"dataset":"dataset"}`+"\n", out.String())
+
+	err = startstop.Stop(graph.Objects(), nil)
+	assert.NoError(t, err)
 }
