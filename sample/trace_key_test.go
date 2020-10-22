3d5adf969154a32f5d642b8b2efdfe97c157b396
Martin Holman <martin308@gmail.com>
2020-10-22 15:02:33 -0700

    Addtional tests around condensing mutliple values

--- sample/trace_key_test.go
+++ sample/trace_key_test.go
@@ -78,4 +78,96 @@ func TestKeyGeneration(t *testing.T) {
 	expected = "2•,200•,true•,/{slug}/home•,4"
 
 	assert.Equal(t, expected, generator.build(trace))
+
+	// now test that multiple values across spans are condensed correctly
+	fields = []string{"http.status_code"}
+	addKey = true
+	key = "meta.key"
+	useTraceLength = true
+
+	generator = newTraceKey(fields, useTraceLength, addKey, key)
+
+	trace = &types.Trace{}
+
+	trace.AddSpan(&types.Span{
+		Event: types.Event{
+			Data: map[string]interface{}{
+				"http.status_code": 200,
+			},
+		},
+	})
+
+	trace.AddSpan(&types.Span{
+		Event: types.Event{
+			Data: map[string]interface{}{
+				"http.status_code": 200,
+			},
+		},
+	})
+
+	trace.AddSpan(&types.Span{
+		Event: types.Event{
+			Data: map[string]interface{}{
+				"http.status_code": 404,
+			},
+		},
+	})
+
+	trace.AddSpan(&types.Span{
+		Event: types.Event{
+			Data: map[string]interface{}{
+				"http.status_code": 404,
+			},
+		},
+	})
+
+	expected = "200•404•,4"
+
+	assert.Equal(t, expected, generator.build(trace))
+
+	// now test that multiple values across spans in a different order are condensed the same
+	fields = []string{"http.status_code"}
+	addKey = true
+	key = "meta.key"
+	useTraceLength = true
+
+	generator = newTraceKey(fields, useTraceLength, addKey, key)
+
+	trace = &types.Trace{}
+
+	trace.AddSpan(&types.Span{
+		Event: types.Event{
+			Data: map[string]interface{}{
+				"http.status_code": 404,
+			},
+		},
+	})
+
+	trace.AddSpan(&types.Span{
+		Event: types.Event{
+			Data: map[string]interface{}{
+				"http.status_code": 404,
+			},
+		},
+	})
+
+	trace.AddSpan(&types.Span{
+		Event: types.Event{
+			Data: map[string]interface{}{
+				"http.status_code": 200,
+			},
+		},
+	})
+
+	trace.AddSpan(&types.Span{
+		Event: types.Event{
+			Data: map[string]interface{}{
+				"http.status_code": 200,
+			},
+		},
+	})
+
+	expected = "200•404•,4"
+
+	assert.Equal(t, expected, generator.build(trace))
 }
