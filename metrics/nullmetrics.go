f95e964824bb1a21299f69bbbb5f26cfd0af5e14
Mike Goldsmth <goldsmith.mike@gmail.com>
2021-03-22 11:21:13 +0000

    add missing Count func to NullMetrics

--- metrics/nullmetrics.go
+++ metrics/nullmetrics.go
@@ -9,4 +9,5 @@ func (n *NullMetrics) Start() {}
 func (n *NullMetrics) Register(name string, metricType string) {}
 func (n *NullMetrics) Increment(name string)                   {}
 func (n *NullMetrics) Gauge(name string, val float64)          {}
+func (n *NullMetrics) Count(name string, n interface{})        {}
 func (n *NullMetrics) Histogram(name string, obs float64)      {}
