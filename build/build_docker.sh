bd5628a84cdb7b040a5fa11cb073c50c058c05b7
Mike Goldsmth <goldsmith.mike@gmail.com>
2021-09-29 13:24:22 +0100

    update build script

--- build/build_docker.sh
+++ build/build_docker.sh
@@ -1,18 +1,20 @@
 set -o nounset
 set -o pipefail
 
-VERSION=${VERSION:-dev}
-PLATFORM="${PLATFORM:-linux/amd64,linux/arm64,darwin/amd64}"
+VERSION="${VERSION:-dev}"
+TAGS="latest"
+if [[ $VERSION != "dev" ]]; then
+    TAGS+=",$VERSION"
+fi
 
 unset GOOS
 unset GOARCH
 export KO_DOCKER_REPO=${KO_DOCKER_REPO:-ko.local}
-export GOFLAGS="-ldflags=-X=main.version=${VERSION}"
+export GOFLAGS="-ldflags=-X=main.version=$VERSION"
 export SOURCE_DATE_EPOCH=$(date +%s)
 # shellcheck disable=SC2086
 ko publish \
-  --tags "head,${VERSION}" \
+  --tags "${TAGS}" \
   --base-import-paths \
-  --platform "${PLATFORM}" \
-  ${PUBLISH_ARGS-} \
+  --platform "linux/amd64,linux/arm64,darwin/amd64" \
   ./cmd/refinery
