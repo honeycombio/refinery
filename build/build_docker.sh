215afe64353b5fc086bdd9d5dd0359b129d463f5
Mike Goldsmth <goldsmith.mike@gmail.com>
2021-10-01 13:34:10 +0100

    run docker publish on all builds

--- build/build_docker.sh
+++ build/build_docker.sh
@@ -2,15 +2,18 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-VERSION="${VERSION:-dev}"
 TAGS="latest"
+VERSION=${CIRCLE_TAG:-dev}
+REPO=${KO_DOCKER_REPO:-ko.local}
 if [[ $VERSION != "dev" ]]; then
+    # set docker username and add version tag
+    REPO="honeycombio"
     TAGS+=",$VERSION"
 fi
 
 unset GOOS
 unset GOARCH
-export KO_DOCKER_REPO=${KO_DOCKER_REPO:-ko.local}
+export KO_DOCKER_REPO=$REPO
 export GOFLAGS="-ldflags=-X=main.BuildID=$VERSION"
 export SOURCE_DATE_EPOCH=$(date +%s)
 # shellcheck disable=SC2086
