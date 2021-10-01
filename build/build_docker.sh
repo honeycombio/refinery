7b1c8f768bde39d9b1fdef2024cac219608a3794
Mike Goldsmith <goldsmith.mike@gmail.com>
2021-10-01 17:46:58 +0100

    Build and publish multi-arch docker images on tag (#323)
    * remove now-unused docker orb
    
    * update Go to 1.16
        + go.mod to 1.16
        + Circle cimg/go to 1.16
    
    * new docker image build script
        + set BuildID ldflags
        + use ko to perform the multi-arch image build *and* publish
    
    * new docker_publish job
        + runs the new image build script
        + setup_remote_docker so a docker service is available during the job
        + caches the ko installation
        + runs on all build workflow executions
            - if running on a tag, publishes to Docker Hub
            - all other builds, publishes to local docker registry
    
    Co-authored-by: Robb Kidd <robbkidd@honeycomb.io>

---
+++ build/build_docker.sh
@@ -0,0 +1,24 @@
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+TAGS="latest"
+VERSION=${CIRCLE_TAG:-dev}
+REPO=${KO_DOCKER_REPO:-ko.local}
+if [[ $VERSION != "dev" ]]; then
+    # set docker username and add version tag
+    REPO="honeycombio"
+    TAGS+=",$VERSION"
+fi
+
+unset GOOS
+unset GOARCH
+export KO_DOCKER_REPO=$REPO
+export GOFLAGS="-ldflags=-X=main.BuildID=$VERSION"
+export SOURCE_DATE_EPOCH=$(date +%s)
+# shellcheck disable=SC2086
+ko publish \
+  --tags "${TAGS}" \
+  --base-import-paths \
+  --platform "linux/amd64,linux/arm64" \
+  ./cmd/refinery
