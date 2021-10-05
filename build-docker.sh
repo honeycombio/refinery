968acf9c3b5f99b30544ee4128f8dfffd643a7ae
Mike Goldsmth <goldsmith.mike@gmail.com>
2021-10-05 15:18:44 +0100

    update bash script to retag correctly

--- build-docker.sh
+++ build-docker.sh
@@ -9,14 +9,11 @@ if [[ -n ${CIRCLE_TAG:-} ]]; then
     VERSION=${CIRCLE_TAG#"v"}
     # append version to image tags
     TAGS+=",$VERSION"
-    # set ko target repo as docker org name so it creates image named '{org}}/{project}'
-    # and will attempt to publish to docker hub
-    export KO_DOCKER_REPO="honeycombio"
 fi
 
 unset GOOS
 unset GOARCH
-export KO_DOCKER_REPO=${KO_DOCKER_REPO:-ko.local}
+export KO_DOCKER_REPO="ko.local"
 export GOFLAGS="-ldflags=-X=main.BuildID=$VERSION"
 export SOURCE_DATE_EPOCH=$(date +%s)
 # shellcheck disable=SC2086
@@ -26,5 +23,8 @@ ko publish \
   --platform "linux/amd64,linux/arm64" \
   ./cmd/refinery
 
-# retag to correct org/repo
-docker image tag ko.local/refinery honeycombio/refinery:latest
+# update tags to use correct org name
+for TAG in ${TAGS//,/ }
+do
+    docker image tag ko.local/refinery honeycombio/refinery:$TAG
+done
