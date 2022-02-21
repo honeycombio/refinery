419903d97f9a7644d6cd82588fb86857c9d5a502
Vera Reynolds <verareynolds@honeycomb.io>
2022-02-21 13:26:23 -0700

    add raciness test to transmitter

--- transmit/transmit_test.go
+++ transmit/transmit_test.go
@@ -1,14 +1,18 @@
+//go:build all || race
 // +build all race
 
 package transmit
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/facebookgo/inject"
 	"github.com/honeycombio/refinery/config"
 	"github.com/honeycombio/refinery/logger"
 	"github.com/honeycombio/refinery/metrics"
+	"github.com/honeycombio/refinery/types"
 
 	libhoney "github.com/honeycombio/libhoney-go"
 	"github.com/stretchr/testify/assert"
@@ -44,5 +48,37 @@ func TestDependencyInjection(t *testing.T) {
 	}
 	if err := g.Populate(); err != nil {
 		t.Error(err)
+	}
+}
+
+func TestConfigReloadRaciness(t *testing.T) {
+	mockConfig := config.MockConfig{}
+	transmission := &DefaultTransmission{
+		Config:     &mockConfig,
+		Logger:     &logger.NullLogger{},
+		Metrics:    &metrics.NullMetrics{},
+		LibhClient: &libhoney.Client{},
+		Version:    "test",
+	}
+
+	err := transmission.Start()
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(mockConfig.Callbacks))
+
+	// imitate concurrent config reload
+	for i := 0; i < 10; i++ {
+		go func(j int) {
+			mockConfig.Callbacks[0]()
+		}(i)
+
+		go func(j int) {
+			mockConfig.Callbacks[0]()
+		}(i)
+
+		transmission.EnqueueEvent(&types.Event{
+			Context:   context.Background(),
+			Timestamp: time.Time{},
+			Data:      map[string]interface{}{"foo": "bar"},
+		})
 	}
 }
