cab7e53db78dfe8a6908a51e86b86400518e6e84
Yingrong Zhao <22300958+VinozzZ@users.noreply.github.com>
2024-02-15 15:08:14 -0500

    make redis_test.go to work again

--- internal/redis/redis_test.go
+++ internal/redis/redis_test.go
@@ -1,211 +1,261 @@
 package redis_test
 
-// import (
-// 	"context"
-// 	"testing"
-// 	"time"
+import (
+	"context"
+	"testing"
+	"time"
 
-// 	"github.com/gofrs/uuid/v5"
-// 	"github.com/honeycombio/refinery/internal/redis"
-// 	"github.com/jonboulle/clockwork"
-// 	"github.com/stretchr/testify/assert"
+	"github.com/alicebob/miniredis/v2"
+	"github.com/gofrs/uuid/v5"
+	"github.com/honeycombio/refinery/internal/redis"
+	"github.com/jonboulle/clockwork"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
-// 	"github.com/honeycombio/hound/test"
-// )
+func TestAcquireLockWithRetriesCancel(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
 
-// func TestAcquireLockWithRetriesCancel(t *testing.T) {
-// 	t.Parallel()
-// 	ctx := context.Background()
+	h := NewRedisTestHarness(ctx, t)
+	defer h.Stop(ctx)
 
-// 	h := NewRedisTestHarness(ctx, t)
-// 	defer h.Stop(ctx)
+	cacheKey := createArbitraryUniqueKey()
 
-// 	cacheKey := createArbitraryUniqueKey()
+	gotLock, clearLock1 := h.Conn.AcquireLockWithRetries(ctx, cacheKey, time.Minute, 5, time.Second)
+	assert.Equal(t, gotLock, true)
 
-// 	gotLock, clearLock1 := h.Conn.AcquireLockWithRetries(ctx, cacheKey, time.Minute, 5, time.Second)
-// 	assert.Equal(t, gotLock, true)
+	innerCtx, cancel := context.WithCancel(context.Background())
+	var clearLock2 func() error
+	done := make(chan bool, 1)
+	go func() {
+		gotLock, clearLock2 = h.Conn.AcquireLockWithRetries(innerCtx, cacheKey, time.Minute, 5, time.Second)
+		done <- true
+	}()
 
-// 	innerCtx, cancel := context.WithCancel(context.Background())
-// 	var clearLock2 func() error
-// 	done := make(chan bool, 1)
-// 	go func() {
-// 		gotLock, clearLock2 = h.Conn.AcquireLockWithRetries(innerCtx, cacheKey, time.Minute, 5, time.Second)
-// 		done <- true
-// 	}()
+	cancel()
+	<-done
+	assert.Equal(t, gotLock, false) // should not get the lock
+	assert.NoError(t, clearLock2()) // should noop without errors
+	assert.NoError(t, clearLock1())
 
-// 	cancel()
-// 	<-done
-// 	assert.Equal(t, gotLock, false) // should not get the lock
-// 	assert.NoError(t, clearLock2()) // should noop without errors
-// 	assert.NoError(t, clearLock1())
+	gotLock, clearLock3 := h.Conn.AcquireLockWithRetries(ctx, cacheKey, time.Minute, 5, time.Second)
+	assert.Equal(t, gotLock, true)
+	assert.NoError(t, clearLock3())
+}
 
-// 	gotLock, clearLock3 := h.Conn.AcquireLockWithRetries(ctx, cacheKey, time.Minute, 5, time.Second)
-// 	assert.Equal(t, gotLock, true)
-// 	assert.NoError(t, clearLock3())
-// }
+func TestAcquireLockWithRetriesTimeout(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
 
-// func TestAcquireLockWithRetriesTimeout(t *testing.T) {
-// 	t.Parallel()
-// 	ctx := context.Background()
+	h := NewRedisTestHarness(ctx, t)
+	defer h.Stop(ctx)
 
-// 	h := NewRedisTestHarness(ctx, t)
-// 	defer h.Stop(ctx)
+	redis := h.Redis.Client.Get()
+	defer redis.Close()
 
-// 	redis := h.Redis.Client.Get()
-// 	defer redis.Close()
+	cacheKey := createArbitraryUniqueKey()
+	ttl := time.Minute
+	retries := 5
+	retryPause := time.Second
 
-// 	cacheKey := createArbitraryUniqueKey()
-// 	ttl := time.Minute
-// 	retries := 5
-// 	retryPause := time.Second
+	gotLock, clearLock1 := h.Conn.AcquireLockWithRetries(ctx, cacheKey, ttl, retries, retryPause)
+	require.Equal(t, gotLock, true)
 
-// 	gotLock, clearLock1 := h.Conn.AcquireLockWithRetries(ctx, cacheKey, ttl, retries, retryPause)
-// 	test.Equals(t, gotLock, true)
+	var clearLock2 func() error
+	done := make(chan bool, 1)
+	go func() {
+		gotLock, clearLock2 = h.Conn.AcquireLockWithRetries(ctx, cacheKey, ttl, retries, retryPause)
+		done <- true
+	}()
 
-// 	var clearLock2 func() error
-// 	done := make(chan bool, 1)
-// 	go func() {
-// 		gotLock, clearLock2 = h.Conn.AcquireLockWithRetries(ctx, cacheKey, ttl, retries, retryPause)
-// 		done <- true
-// 	}()
+	// iterate through all the retries
+	for i := 0; i < retries; i++ {
+		h.Clock.BlockUntil(1)
+		h.Clock.Advance(retryPause)
+	}
 
-// 	// iterate through all the retries
-// 	for i := 0; i < retries; i++ {
-// 		h.Clock.BlockUntil(1)
-// 		h.Clock.Advance(retryPause)
-// 	}
+	<-done
+	require.False(t, gotLock)        // should not get the lock
+	require.NoError(t, clearLock2()) // should noop without errors
+	require.NoError(t, clearLock1())
 
-// 	<-done
-// 	test.Equals(t, gotLock, false) // should not get the lock
-// 	test.OK(t, clearLock2())       // should noop without errors
-// 	test.OK(t, clearLock1())
+	gotLock, clearLock3 := h.Conn.AcquireLockWithRetries(ctx, cacheKey, ttl, retries, retryPause)
+	require.True(t, gotLock)
+	require.NoError(t, clearLock3())
+}
 
-// 	gotLock, clearLock3 := h.Conn.AcquireLockWithRetries(ctx, cacheKey, ttl, retries, retryPause)
-// 	test.Equals(t, gotLock, true)
-// 	test.OK(t, clearLock3())
-// }
+func TestAcquireLockWithRetriesTTLExceeded(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
 
-// func TestAcquireLockWithRetriesTTLExceeded(t *testing.T) {
-// 	t.Parallel()
-// 	ctx := context.Background()
+	h := NewRedisTestHarness(ctx, t)
+	defer h.Stop(ctx)
 
-// 	h := NewRedisTestHarness(ctx, t)
-// 	defer h.Stop(ctx)
+	redis := h.Redis.Client.Get()
+	defer redis.Close()
 
-// 	redis := h.Redis.Client.Get()
-// 	defer redis.Close()
+	cacheKey := createArbitraryUniqueKey()
+	ttl := time.Minute
+	retries := 5
+	retryPause := time.Second
 
-// 	cacheKey := createArbitraryUniqueKey()
-// 	ttl := time.Minute
-// 	retries := 5
-// 	retryPause := time.Second
+	gotLock, clearLock1 := h.Conn.AcquireLockWithRetries(ctx, cacheKey, ttl, retries, retryPause)
+	require.True(t, gotLock)
 
-// 	gotLock, clearLock1 := h.Conn.AcquireLockWithRetries(ctx, cacheKey, ttl, retries, retryPause)
-// 	test.Equals(t, gotLock, true)
+	var clearLock2 func() error
+	done := make(chan bool, 1)
+	go func() {
+		gotLock, clearLock2 = h.Conn.AcquireLockWithRetries(ctx, cacheKey, ttl, retries, retryPause)
+		done <- true
+	}()
 
-// 	var clearLock2 func() error
-// 	done := make(chan bool, 1)
-// 	go func() {
-// 		gotLock, clearLock2 = h.Conn.AcquireLockWithRetries(ctx, cacheKey, ttl, retries, retryPause)
-// 		done <- true
-// 	}()
+	// wait until redis TTL
+	h.Clock.BlockUntil(1)
+	h.Clock.Advance(retryPause)
+	h.Redis.Server.FastForward(ttl)
 
-// 	// wait until redis TTL
-// 	h.Clock.BlockUntil(1)
-// 	h.Clock.Advance(retryPause)
-// 	h.Redis.Server.FastForward(ttl)
+	<-done
+	require.True(t, gotLock)        // should get the lock
+	assert.NoError(t, clearLock2()) // should clear the lock
+	assert.Error(t, clearLock1())   // should error because the lock is gone (due to TTL)
 
-// 	<-done
-// 	test.Equals(t, gotLock, true) // should get the lock
-// 	test.OK(t, clearLock2())      // should clear the lock
-// 	test.Err(t, clearLock1())     // should error because the lock is gone (due to TTL)
+	gotLock, clearLock3 := h.Conn.AcquireLockWithRetries(ctx, cacheKey, ttl, retries, retryPause)
+	require.True(t, gotLock)
+	require.NoError(t, clearLock3())
+}
 
-// 	gotLock, clearLock3 := h.Conn.AcquireLockWithRetries(ctx, cacheKey, ttl, retries, retryPause)
-// 	test.Equals(t, gotLock, true)
-// 	test.OK(t, clearLock3())
-// }
+func Test_MGetSetings(t *testing.T) {
+	ctx := context.Background()
 
-// func Test_MGetSetings(t *testing.T) {
-// 	ctx := context.Background()
+	h := NewRedisTestHarness(ctx, t)
+	defer h.Stop(ctx)
 
-// 	h := NewRedisTestHarness(ctx, t)
-// 	defer h.Stop(ctx)
+	redis := h.Redis.Client.Get()
+	defer redis.Close()
 
-// 	redis := h.Redis.Client.Get()
-// 	defer redis.Close()
+	redis.SetString("foo", "fooval")
+	redis.SetString("bar", "barval")
 
-// 	redis.SetString("foo", "fooval")
-// 	redis.SetString("bar", "barval")
+	vals, err := redis.MGetStrings("foo", "bar", "baz")
+	require.NoError(t, err)
+	require.EqualValues(t, []string{"fooval", "barval", ""}, vals)
+}
 
-// 	vals, err := redis.MGetStrings("foo", "bar", "baz")
-// 	test.Nil(t, err)
-// 	test.Equals(t, []string{"fooval", "barval", ""}, vals)
-// }
+func Test_SetStringTTL(t *testing.T) {
+	ctx := context.Background()
 
-// func Test_SetStringTTL(t *testing.T) {
-// 	ctx := context.Background()
+	h := NewRedisTestHarness(ctx, t)
+	defer h.Stop(ctx)
 
-// 	h := NewRedisTestHarness(ctx, t)
-// 	defer h.Stop(ctx)
+	redis := h.Redis.Client.Get()
+	defer redis.Close()
 
-// 	redis := h.Redis.Client.Get()
-// 	defer redis.Close()
+	ttlDays := 30
+	ttl := time.Duration(ttlDays*24) * time.Hour
 
-// 	ttlDays := 30
-// 	ttl := time.Duration(ttlDays*24) * time.Hour
+	_, err := redis.SetStringTTL(ctx, "foo", "fooval", ttl)
+	require.NoError(t, err)
 
-// 	_, err := redis.SetStringTTL(ctx, "foo", "fooval", ttl)
-// 	test.Nil(t, err)
+	val, err := redis.GetString(ctx, "foo")
+	require.NoError(t, err)
+	require.Equal(t, "fooval", val)
+}
 
-// 	val, err := redis.GetString(ctx, "foo")
-// 	test.Nil(t, err)
-// 	test.Equals(t, "fooval", val)
-// }
+func Test_SetStringsTTL(t *testing.T) {
+	ctx := context.Background()
 
-// func Test_SetStringsTTL(t *testing.T) {
-// 	ctx := context.Background()
+	h := NewRedisTestHarness(ctx, t)
+	defer h.Stop(ctx)
 
-// 	h := NewRedisTestHarness(ctx, t)
-// 	defer h.Stop(ctx)
+	redis := h.Redis.Client.Get()
+	defer redis.Close()
 
-// 	redis := h.Redis.Client.Get()
-// 	defer redis.Close()
+	ttlDays := 30
+	ttl := time.Duration(ttlDays*24) * time.Hour
 
-// 	ttlDays := 30
-// 	ttl := time.Duration(ttlDays*24) * time.Hour
+	_, err := redis.SetStringsTTL([]string{"foo", "bar"}, []string{"fooval", "barval"}, ttl)
+	require.NoError(t, err)
 
-// 	_, err := redis.SetStringsTTL([]string{"foo", "bar"}, []string{"fooval", "barval"}, ttl)
-// 	test.Nil(t, err)
+	vals, err := redis.GetStrings("foo", "bar", "baz")
+	require.NoError(t, err)
+	require.EqualValues(t, []string{"fooval", "barval", ""}, vals)
+}
 
-// 	vals, err := redis.GetStrings("foo", "bar", "baz")
-// 	test.Nil(t, err)
-// 	test.Equals(t, []string{"fooval", "barval", ""}, vals)
-// }
+type CachingClientHarness struct {
+	Clock clockwork.FakeClock
+	Redis TestRedis
+	Conn  redis.Conn
+}
 
-// type CachingClientHarness struct {
-// 	Clock clockwork.FakeClock
-// 	Redis hnytest.TestRedis
-// 	Conn  redis.Conn
-// }
+func NewRedisTestHarness(ctx context.Context, t *testing.T) *CachingClientHarness {
+	h := &CachingClientHarness{}
 
-// func NewRedisTestHarness(ctx context.Context, t *testing.T) *CachingClientHarness {
-// 	h := &CachingClientHarness{}
+	h.Redis = TestRedis{}
+	h.Redis.Start(ctx)
 
-// 	h.Redis = hnytest.TestRedis{}
-// 	h.Redis.Start(ctx)
+	conn := h.Redis.Client.Get().(*RedisConnStub)
+	h.Clock = conn.DefaultConn.Clock.(clockwork.FakeClock)
+	h.Conn = conn
 
-// 	conn := h.Redis.Client.Get().(*hnytest.RedisConnStub)
-// 	h.Clock = conn.DefaultConn.Clock.(clockwork.FakeClock)
-// 	h.Conn = conn
+	return h
+}
 
-// 	return h
-// }
+func (h *CachingClientHarness) Stop(ctx context.Context) {
+	h.Conn.Close()
+	h.Redis.Stop(context.Background())
+}
 
-// func (h *CachingClientHarness) Stop(ctx context.Context) {
-// 	h.Conn.Close()
-// 	h.Redis.Stop(context.Background())
-// }
+func createArbitraryUniqueKey() string {
+	return uuid.Must(uuid.NewV4()).String()
+}
 
-// func createArbitraryUniqueKey() string {
-// 	return uuid.Must(uuid.NewV4()).String()
-// }
+type TestRedis struct {
+	Server                     *miniredis.Miniredis
+	Client                     redis.Client
+	MockAcquireLockWithRetries func(context.Context, string, time.Duration, int, time.Duration) (bool, func() error)
+}
+
+func (tr *TestRedis) Start(ctx context.Context) {
+	tr.Server, _ = miniredis.Run()
+	tr.Client = &RedisClientStub{
+		Client:    redis.NewClient(&redis.Config{Addr: tr.Server.Addr()}),
+		testRedis: tr,
+	}
+}
+
+func (tr *TestRedis) Stop(ctx context.Context) error {
+	err := tr.Client.Stop(ctx)
+	tr.Server.Close()
+	return err
+}
+
+type RedisClientStub struct {
+	redis.Client
+	testRedis *TestRedis
+}
+
+func (s *RedisClientStub) Get() redis.Conn {
+	conn := s.Client.Get().(*redis.DefaultConn)
+	conn.Clock = clockwork.NewFakeClock()
+	return &RedisConnStub{
+		DefaultConn: *conn,
+		testRedis:   s.testRedis,
+	}
+}
+
+type RedisConnStub struct {
+	redis.DefaultConn
+	testRedis *TestRedis
+}
+
+func (s *RedisConnStub) AcquireLockWithRetries(ctx context.Context, key string, ttl time.Duration, maxRetries int, retryInterval time.Duration) (bool, func() error) {
+	if s.testRedis.MockAcquireLockWithRetries != nil {
+		return s.testRedis.MockAcquireLockWithRetries(ctx, key, ttl, maxRetries, retryInterval)
+	}
+	return s.DefaultConn.AcquireLockWithRetries(ctx, key, ttl, maxRetries, retryInterval)
+}
+
+func BadRedis() redis.Client {
+	return redis.NewClient(&redis.Config{Addr: "localhost:9"})
+}
