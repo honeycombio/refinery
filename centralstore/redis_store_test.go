da8c87cb67342671f0c8d1c3301a0d6a66faaaff
Yingrong Zhao <22300958+VinozzZ@users.noreply.github.com>
2024-02-23 15:29:50 -0500

    have traceStateProcessor to init its own dep

--- centralstore/redis_store_test.go
+++ centralstore/redis_store_test.go
@@ -8,8 +8,12 @@ import (
 	"time"
 
 	"github.com/alicebob/miniredis/v2"
+	"github.com/honeycombio/refinery/collect/cache"
+	"github.com/honeycombio/refinery/config"
 	"github.com/honeycombio/refinery/internal/redis"
+	"github.com/honeycombio/refinery/metrics"
 	"github.com/jonboulle/clockwork"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -73,6 +77,7 @@ func TestRedisBasicStore_TraceStateProcessor(t *testing.T) {
 
 				require.True(t, ts.exists(conn, change.to, traceID))
 				require.False(t, ts.exists(conn, tc.state, traceID))
+				//TODO: make sure the timestamp is updated
 			}
 		})
 	}
@@ -84,17 +89,29 @@ func TestRedisBasicStore_ConcurrentStateChange(t *testing.T) {
 	defer redisClient.Stop(ctx)
 
 	traceID := "traceID0"
-	ts := newTestTraceStateProcessor(t, redisClient)
+	store := NewTestRedisBasicStore(t, redisClient)
+	defer store.Stop()
+
+	require.NoError(t, store.WriteSpan(&CentralSpan{
+		TraceID:   traceID,
+		SpanID:    "spanID0",
+		ParentID:  traceID,
+		KeyFields: map[string]interface{}{"foo": "bar"},
+		IsRoot:    false,
+	}))
+
+	status, err := store.GetStatusForTraces([]string{traceID})
+	require.NoError(t, err)
+	require.Len(t, status, 1)
+	initialTimestamp := status[0].Timestamp
 
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			conn := redisClient.Get()
-			defer conn.Close()
-			_ = ts.addNewTrace(conn, traceID)
-			_ = ts.toNextState(conn, newTraceStateChangeEvent(Collecting, WaitingToDecide), traceID)
+			_ = store.ChangeTraceStatus([]string{traceID}, Collecting, WaitingToDecide)
+			_ = store.ChangeTraceStatus([]string{traceID}, WaitingToDecide, ReadyForDecision)
 		}()
 	}
 
@@ -104,9 +121,9 @@ func TestRedisBasicStore_ConcurrentStateChange(t *testing.T) {
 		conn := redisClient.Get()
 		defer conn.Close()
 
-		_ = ts.addNewTrace(conn, traceID)
-		_ = ts.toNextState(conn, newTraceStateChangeEvent(Collecting, WaitingToDecide), traceID)
-		_ = ts.toNextState(conn, newTraceStateChangeEvent(WaitingToDecide, ReadyForDecision), traceID)
+		_ = store.ChangeTraceStatus([]string{traceID}, Collecting, WaitingToDecide)
+		_ = store.ChangeTraceStatus([]string{traceID}, WaitingToDecide, ReadyForDecision)
+		_ = store.ChangeTraceStatus([]string{traceID}, ReadyForDecision, AwaitingDecision)
 	}()
 
 	wg.Wait()
@@ -114,9 +131,18 @@ func TestRedisBasicStore_ConcurrentStateChange(t *testing.T) {
 	conn := redisClient.Get()
 	defer conn.Close()
 
-	require.False(t, ts.exists(conn, Collecting, traceID))
-	require.False(t, ts.exists(conn, WaitingToDecide, traceID))
-	require.True(t, ts.exists(conn, ReadyForDecision, traceID))
+	require.False(t, store.states.exists(conn, Collecting, traceID))
+	require.False(t, store.states.exists(conn, WaitingToDecide, traceID))
+	require.False(t, store.states.exists(conn, ReadyForDecision, traceID))
+	require.True(t, store.states.exists(conn, AwaitingDecision, traceID))
+	//TODO: make sure the timestamp is updated
+
+	status, err = store.GetStatusForTraces([]string{traceID})
+	require.NoError(t, err)
+	require.Len(t, status, 1)
+	assert.Equal(t, AwaitingDecision, status[0].State)
+	assert.True(t, status[0].Timestamp.After(initialTimestamp))
+
 }
 
 func TestRedisBasicStore_Cleanup(t *testing.T) {
@@ -172,6 +198,78 @@ func TestRedisBasicStore_normalizeCentralTraceStatusRedis(t *testing.T) {
 
 }
 
+func TestRedisBasicStore_TraceStatus(t *testing.T) {
+	// make sure the trace status is not override by spans arrived after the trace status is set
+	ctx := context.Background()
+	redisClient := NewTestRedis()
+	defer redisClient.Stop(ctx)
+
+	statusStore := newTraceStatusStore(clockwork.NewFakeClock())
+
+	conn := redisClient.Get()
+	defer conn.Close()
+
+	traceID0 := "traceID0"
+	span := &CentralSpan{
+		TraceID:   traceID0,
+		SpanID:    "spanID0",
+		ParentID:  traceID0,
+		KeyFields: map[string]interface{}{"foo": "bar"},
+		IsRoot:    false,
+	}
+	require.NoError(t, statusStore.addStatus(conn, span))
+	status, err := statusStore.getTraceStatuses(conn, []string{traceID0})
+	require.NoError(t, err)
+	require.Len(t, status, 1)
+	require.NotNil(t, status[traceID0])
+	assert.Equal(t, Unknown, status[traceID0].State)
+	span0Timestamp := status[traceID0].Timestamp
+	assert.NotNil(t, span0Timestamp)
+
+	span.SpanID = "spanID1"
+	require.NoError(t, statusStore.addStatus(conn, span))
+	status, err = statusStore.getTraceStatuses(conn, []string{traceID0})
+	require.NoError(t, err)
+	require.Len(t, status, 1)
+	require.NotNil(t, status[traceID0])
+	assert.Equal(t, Unknown, status[traceID0].State)
+	span1Timestamp := status[traceID0].Timestamp
+	assert.NotNil(t, span1Timestamp)
+	assert.Equal(t, span0Timestamp, span1Timestamp)
+}
+
+type TestRedisBasicStore struct {
+	*RedisBasicStore
+	clock clockwork.FakeClock
+}
+
+func NewTestRedisBasicStore(t *testing.T, redisClient *TestRedisClient) *TestRedisBasicStore {
+	clock := clockwork.NewFakeClock()
+	opt := RedisBasicStoreOptions{Cache: config.SampleCacheConfig{
+		KeptSize:          100,
+		DroppedSize:       10000,
+		SizeCheckInterval: config.Duration(10 * time.Second),
+	}}
+	decisionCache, err := cache.NewCuckooSentCache(opt.Cache, &metrics.NullMetrics{})
+	require.NoError(t, err)
+
+	ts := newTraceStateProcessor(traceStateProcessorConfig{
+		changeState: redisClient.NewScript(stateChangeKey, stateChangeScript),
+	}, clock)
+	err = ts.Start(context.Background(), redisClient)
+	require.NoError(t, err)
+	return &TestRedisBasicStore{
+		RedisBasicStore: &RedisBasicStore{
+			client:        redisClient,
+			states:        ts,
+			traces:        newTraceStatusStore(clock),
+			decisionCache: decisionCache,
+			errs:          make(chan error, defaultPendingWorkCapacity),
+		},
+		clock: clock,
+	}
+}
+
 type TestRedisClient struct {
 	Server *miniredis.Miniredis
 	redis.Client
@@ -188,23 +286,25 @@ func NewTestRedis() *TestRedisClient {
 	}
 }
 
-func (tr *TestRedisClient) Stop(ctx context.Context) {
+func (tr *TestRedisClient) Stop(ctx context.Context) error {
 	tr.Client.Stop(ctx)
 	tr.Server.Close()
+	return nil
 }
 
 type testTraceStateProcessor struct {
-	*testStateProcessor
+	*traceStateProcessor
 	clock clockwork.FakeClock
 }
 
 func newTestTraceStateProcessor(t *testing.T, redisClient *TestRedisClient) *testTraceStateProcessor {
-	require.NoError(t, ensureValidStateChangeEvents(redisClient.Client))
 	clock := clockwork.NewFakeClock()
-	return &testTraceStateProcessor{
-		testStateProcessor: newTraceStateProcessor(traceStateProcessorConfig{
+	ts := &testTraceStateProcessor{
+		traceStateProcessor: newTraceStateProcessor(traceStateProcessorConfig{
 			changeState: redisClient.NewScript(stateChangeKey, stateChangeScript),
 		}, clock),
 		clock: clock,
 	}
+	require.NoError(t, ts.Start(context.TODO(), redisClient))
+	return ts
 }
