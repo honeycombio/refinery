2823377caa22f8c7ef5af94deb4e05634f0bc4eb
Kent Quirk <kentquirk@honeycomb.io>
2024-03-14 09:54:45 -0400

    WIP for sharing

--- centralstore/redis_store_test.go
+++ centralstore/redis_store_test.go
@@ -361,7 +361,7 @@ func TestRedisBasicStore_Cleanup(t *testing.T) {
 	testRedis.Start()
 	defer testRedis.Stop()
 
-	ts := newTestTraceStateProcessor(t, testRedis, nil, trace.Tracer(noop.NewTracerProvider().Tracer("test")))
+	ts := newTestTraceStateProcessor(t, testRedis, nil, noop.NewTracerProvider().Tracer("test"))
 	ts.config.maxTraceRetention = 1 * time.Minute
 	ts.config.reaperRunInterval = 500 * time.Millisecond
 	require.NoError(t, ts.init(testRedis))
@@ -397,7 +397,7 @@ func TestRedisBasicStore_ValidStateTransition(t *testing.T) {
 	defer testRedis.Stop()
 
 	traceID := "traceID0"
-	ts := newTestTraceStateProcessor(t, testRedis, nil, trace.Tracer(noop.NewTracerProvider().Tracer("test")))
+	ts := newTestTraceStateProcessor(t, testRedis, nil, noop.NewTracerProvider().Tracer("test"))
 	require.NoError(t, ts.init(testRedis))
 	defer ts.Stop()
 
