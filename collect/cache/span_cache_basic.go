74467d3789c7a0fa57f827f346a7629175bd158b
Kent Quirk <kentquirk@honeycomb.io>
2024-03-26 19:54:09 -0400

    basic and complex caches for experimentation

---
+++ collect/cache/span_cache_basic.go
@@ -0,0 +1,122 @@
+package cache
+
+import (
+	"sync"
+
+	"github.com/facebookgo/startstop"
+	"github.com/honeycombio/refinery/types"
+	"github.com/jonboulle/clockwork"
+)
+
+// this is a naive implementation that uses a map
+// and doesn't try to be clever about memory usage or allocations
+type spanCache_basic struct {
+	Clock   clockwork.Clock `inject:""`
+	cache   map[string]*types.Trace
+	current []string
+	nextix  int
+	mut     sync.RWMutex
+}
+
+// ensure that spanCache implements SpanCache
+var _ SpanCache = &spanCache_basic{}
+
+// ensure that spanCache implements startstop.Starter
+var _ startstop.Starter = &spanCache_basic{}
+
+// ensure that spanCache implements startstop.Stopper
+var _ startstop.Stopper = &spanCache_basic{}
+
+func (sc *spanCache_basic) Start() error {
+	sc.cache = make(map[string]*types.Trace)
+	return nil
+}
+
+func (sc *spanCache_basic) Stop() error {
+	sc.cache = nil
+	return nil
+}
+
+func (sc *spanCache_basic) GetClock() clockwork.Clock {
+	return sc.Clock
+}
+
+func (sc *spanCache_basic) Set(sp *types.Span) error {
+	sc.mut.Lock()
+	defer sc.mut.Unlock()
+	traceID := sp.TraceID
+	trace, ok := sc.cache[traceID]
+	if !ok {
+		trace = &types.Trace{
+			APIHost:     sp.APIHost,
+			APIKey:      sp.APIKey,
+			Dataset:     sp.Dataset,
+			TraceID:     traceID,
+			ArrivalTime: sc.Clock.Now(),
+		}
+		sc.cache[traceID] = trace
+	}
+	trace.AddSpan(sp)
+	return nil
+}
+
+func (sc *spanCache_basic) Get(traceID string) *types.Trace {
+	sc.mut.RLock()
+	defer sc.mut.RUnlock()
+	trace, ok := sc.cache[traceID]
+	if !ok {
+		return nil
+	}
+	return trace
+}
+
+// doesn't bother to sort the spans by arrival time, just returns the
+// appropriate fraction of traces
+func (sc *spanCache_basic) GetOldest(fract float64) []string {
+	n := int(float64(len(sc.cache)) * fract)
+	ret := make([]string, 0, n)
+	sc.mut.RLock()
+	i := 0
+	for traceID := range sc.cache {
+		ret = append(ret, traceID)
+		i++
+		if i >= n {
+			break
+		}
+	}
+	sc.mut.RUnlock()
+	return ret
+}
+
+// This gets a batch of up to n traceIDs from the cache; it's used to get a
+// batch of traceIDs to process in parallel. It snapshots the active map and
+// returns a slice of traceIDs that were current at the time of the call. It
+// will return successive slices of traceIDs until it has returned all of them,
+// then it will start over from a fresh snapshot.
+func (sc *spanCache_basic) GetTraceIDs(n int) []string {
+	// this is the only function that looks at current or nextix so it
+	// doesn't need to lock those fields
+	if sc.current == nil || sc.nextix >= len(sc.current) {
+		sc.mut.RLock()
+		sc.current = make([]string, 0, len(sc.cache))
+		for traceID := range sc.cache {
+			sc.current = append(sc.current, traceID)
+		}
+		sc.mut.RUnlock()
+		sc.nextix = 0
+	}
+	if sc.nextix+n > len(sc.current) {
+		n = len(sc.current) - sc.nextix
+	}
+	return sc.current[sc.nextix : sc.nextix+n]
+}
+
+func (sc *spanCache_basic) Remove(traceID string) {
+	sc.mut.Lock()
+	defer sc.mut.Unlock()
+	delete(sc.cache, traceID)
+}
+
+func (sc *spanCache_basic) Len() int {
+	return len(sc.cache)
+}
--- collect/cache/span_cache_test.go
+++ collect/cache/span_cache_test.go
@@ -1,106 +1,247 @@
 package cache
 
 import (
+	"math/rand"
 	"testing"
 
+	"github.com/facebookgo/startstop"
+	"github.com/honeycombio/refinery/config"
 	"github.com/honeycombio/refinery/types"
 	"github.com/jonboulle/clockwork"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestXxx(t *testing.T) {
-	c := spanCache{
-		Clock: clockwork.NewFakeClock(),
-	}
-	err := c.Start()
-	require.NoError(t, err)
-	defer c.Stop()
-
-	// test that we can add a span
-	span := &types.Span{
-		TraceID: "trace1",
-		Event: types.Event{
-			APIHost: "apihost",
-			APIKey:  "apikey",
-			Dataset: "dataset",
+func getCache(typ string, clock clockwork.Clock) SpanCache {
+	cfg := &config.MockConfig{
+		GetCollectionConfigVal: config.CollectionConfig{
+			CacheCapacity: 10000,
 		},
 	}
-	err = c.Set(span)
-	require.NoError(t, err)
+	switch typ {
+	case "basic":
+		return &spanCache_basic{
+			Clock: clock,
+		}
+	case "complex":
+		return &spanCache_complex{
+			Cfg:   cfg,
+			Clock: clock,
+		}
+	default:
+		panic("unknown cache type")
+	}
+}
 
-	// test that we can retrieve the span
-	trace, err := c.Get("trace1")
-	require.NoError(t, err)
-	assert.Equal(t, "trace1", trace.TraceID)
-	assert.Equal(t, "apihost", trace.APIHost)
-	assert.Equal(t, "apikey", trace.APIKey)
-	assert.Equal(t, "dataset", trace.Dataset)
-	assert.Equal(t, c.Clock.Now(), trace.ArrivalTime)
-	assert.Equal(t, 1, c.Len())
+func TestSpanCache(t *testing.T) {
+	for _, typ := range []string{"basic", "complex"} {
+		c := getCache(typ, clockwork.NewFakeClock())
+		t.Run(typ, func(t *testing.T) {
 
-	// test that we can remove the span
-	err = c.Remove("trace1")
-	require.NoError(t, err)
-	_, err = c.Get("trace1")
-	require.NoError(t, err) // should not return an error
-	require.Equal(t, 0, c.Len())
+			err := c.(startstop.Starter).Start()
+			require.NoError(t, err)
+			defer c.(startstop.Stopper).Stop()
+
+			// test that we can add a span
+			span := &types.Span{
+				TraceID: "trace1",
+				Event: types.Event{
+					APIHost: "apihost",
+					APIKey:  "apikey",
+					Dataset: "dataset",
+				},
+			}
+			err = c.Set(span)
+			require.NoError(t, err)
+
+			// test that we can retrieve the span
+			trace := c.Get("trace1")
+			assert.Equal(t, "trace1", trace.TraceID)
+			assert.Equal(t, "apihost", trace.APIHost)
+			assert.Equal(t, "apikey", trace.APIKey)
+			assert.Equal(t, "dataset", trace.Dataset)
+			// assert.Equal(t, c.Clock.Now(), trace.ArrivalTime)
+			assert.Equal(t, 1, c.Len())
+
+			// test that we can remove the span
+			c.Remove("trace1")
+			c.Get("trace1")
+			require.Equal(t, 0, c.Len())
+		})
+	}
 }
 
 func BenchmarkSpanCacheAdd(b *testing.B) {
-	c := spanCache{
-		Clock: clockwork.NewFakeClock(),
-	}
-	c.Start()
-	defer c.Stop()
+	for _, typ := range []string{"basic", "complex"} {
+		c := getCache(typ, clockwork.NewFakeClock())
+		b.Run(typ, func(b *testing.B) {
 
-	ids := make([]string, b.N)
-	for i := 0; i < b.N; i++ {
-		ids[i] = genID(32)
-	}
-	evt := types.Event{
-		APIHost: "apihost",
-		APIKey:  "apikey",
-		Dataset: "dataset",
-	}
-	b.ResetTimer()
+			c.(startstop.Starter).Start()
+			defer c.(startstop.Stopper).Stop()
 
-	for i := 0; i < b.N; i++ {
-		span := &types.Span{
-			TraceID: "",
-			Event:   evt,
-		}
-		c.Set(span)
+			ids := make([]string, b.N)
+			for i := 0; i < b.N; i++ {
+				ids[i] = genID(32)
+			}
+			evt := types.Event{
+				APIHost: "apihost",
+				APIKey:  "apikey",
+				Dataset: "dataset",
+			}
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				span := &types.Span{
+					TraceID: ids[i],
+					Event:   evt,
+				}
+				c.Set(span)
+			}
+		})
 	}
 }
 
 func BenchmarkSpanCacheGet(b *testing.B) {
-	c := spanCache{
-		Clock: clockwork.NewFakeClock(),
-	}
-	c.Start()
-	defer c.Stop()
+	for _, typ := range []string{"basic", "complex"} {
+		c := getCache(typ, clockwork.NewFakeClock())
+		b.Run(typ, func(b *testing.B) {
 
-	ids := make([]string, b.N)
-	for i := 0; i < b.N; i++ {
-		ids[i] = genID(32)
-	}
-	evt := types.Event{
-		APIHost: "apihost",
-		APIKey:  "apikey",
-		Dataset: "dataset",
-	}
+			c.(startstop.Starter).Start()
+			defer c.(startstop.Stopper).Stop()
 
-	for i := 0; i < b.N; i++ {
-		span := &types.Span{
-			TraceID: "",
-			Event:   evt,
-		}
-		c.Set(span)
-	}
-	b.ResetTimer()
+			ids := make([]string, b.N)
+			for i := 0; i < b.N; i++ {
+				ids[i] = genID(32)
+			}
+			evt := types.Event{
+				APIHost: "apihost",
+				APIKey:  "apikey",
+				Dataset: "dataset",
+			}
 
-	for i := 0; i < b.N; i++ {
-		c.Get(ids[i])
+			for i := 0; i < b.N; i++ {
+				span := &types.Span{
+					TraceID: ids[i],
+					Event:   evt,
+				}
+				c.Set(span)
+			}
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				c.Get(ids[i])
+			}
+		})
+	}
+}
+
+func BenchmarkSpanCacheGetTraceIDs(b *testing.B) {
+	for _, typ := range []string{"basic", "complex"} {
+		c := getCache(typ, clockwork.NewFakeClock())
+		b.Run(typ, func(b *testing.B) {
+
+			c.(startstop.Starter).Start()
+			defer c.(startstop.Stopper).Stop()
+
+			ids := make([]string, b.N)
+			for i := 0; i < b.N; i++ {
+				ids[i] = genID(32)
+			}
+			evt := types.Event{
+				APIHost: "apihost",
+				APIKey:  "apikey",
+				Dataset: "dataset",
+			}
+
+			for i := 0; i < b.N; i++ {
+				span := &types.Span{
+					TraceID: ids[i],
+					Event:   evt,
+				}
+				c.Set(span)
+			}
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				c.GetTraceIDs(50)
+			}
+		})
+	}
+}
+func BenchmarkSpanCacheGetOldest(b *testing.B) {
+	for _, typ := range []string{"basic", "complex"} {
+		c := getCache(typ, clockwork.NewRealClock())
+		b.Run(typ, func(b *testing.B) {
+
+			c.(startstop.Starter).Start()
+			defer c.(startstop.Stopper).Stop()
+
+			ids := make([]string, b.N)
+			for i := 0; i < b.N; i++ {
+				ids[i] = genID(32)
+			}
+			evt := types.Event{
+				APIHost: "apihost",
+				APIKey:  "apikey",
+				Dataset: "dataset",
+			}
+
+			for i := 0; i < b.N; i++ {
+				span := &types.Span{
+					TraceID:     ids[i],
+					Event:       evt,
+					ArrivalTime: c.GetClock().Now(),
+				}
+				c.Set(span)
+			}
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				c.GetOldest(.10)
+			}
+		})
+	}
+}
+
+func BenchmarkSpanCacheMixed(b *testing.B) {
+	const numIDs = 10000
+	for _, typ := range []string{"basic", "complex"} {
+		c := getCache(typ, clockwork.NewFakeClock())
+		b.Run(typ, func(b *testing.B) {
+
+			c.(startstop.Starter).Start()
+			defer c.(startstop.Stopper).Stop()
+
+			ids := make([]string, numIDs)
+			for i := 0; i < numIDs; i++ {
+				ids[i] = genID(32)
+			}
+			evt := types.Event{
+				APIHost: "apihost",
+				APIKey:  "apikey",
+				Dataset: "dataset",
+			}
+
+			b.ResetTimer()
+			// we have numIDs IDs, and we'll iterate and for each
+			// ID we'll either:
+			// - add a span (80% of the time)
+			// - get a trace (10% of the time)
+			// - delete a trace (10% of the time)
+			for i := 0; i < b.N; i++ {
+				switch rand.Intn(10) {
+				case 0:
+					c.Remove(ids[i%numIDs])
+				case 1:
+					c.Get(ids[i%numIDs])
+				default:
+					span := &types.Span{
+						TraceID: ids[i%numIDs],
+						Event:   evt,
+					}
+					c.Set(span)
+				}
+			}
+		})
 	}
 }
