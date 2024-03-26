befd0875c3b1652a6fc7d5e07e3e133deae32268
Kent Quirk <kentquirk@honeycomb.io>
2024-03-26 19:54:09 -0400

    basic and complex caches for experimentation

--- collect/cache/span_cache.go
+++ collect/cache/span_cache.go
@@ -1,91 +1,30 @@
 package cache
 
 import (
-	"github.com/facebookgo/startstop"
 	"github.com/honeycombio/refinery/types"
 	"github.com/jonboulle/clockwork"
 )
 
 type SpanCache interface {
+	// Adds a span to the cache. If a trace with the same traceID already exists,
+	// the span will be added to that trace. If no trace with the traceID exists,
+	// a new trace will be created. Only returns an error if there is a problem
+	// adding the span to the cache.
 	Set(span *types.Span) error
-	Get(traceID string) (*types.Trace, error)
-	GetOldest(n int) []*types.Trace
-	Remove(traceID string) error
+	// Retrieves a trace from the cache by traceID. If no trace with the traceID
+	// exists, returns nil.
+	Get(traceID string) *types.Trace
+	// Returns the desired fraction of oldest trace IDs in the cache.
+	// (e.g. if fract is 0.1, returns the oldest 10% of trace IDs)
+	GetOldest(fract float64) []string
+	// Returns up to n trace IDs from the cache; successive calls will return
+	// different trace IDs until all have been returned, then it will start over.
+	GetTraceIDs(n int) []string
+	// Removes a trace from the cache by traceID. If no trace with the traceID exists,
+	// does nothing.
+	Remove(traceID string)
+	// Returns the number of traces in the cache.
 	Len() int
-}
-
-// we're going to start with a naive implementation that uses a map
-// and doesn't try to be clever about memory usage or allocations
-type spanCache struct {
-	Clock clockwork.Clock `inject:""`
-	cache map[string]*types.Trace
-}
-
-// ensure that spanCache implements SpanCache
-var _ SpanCache = &spanCache{}
-
-// ensure that spanCache implements startstop.Starter
-var _ startstop.Starter = &spanCache{}
-
-// ensure that spanCache implements startstop.Stopper
-var _ startstop.Stopper = &spanCache{}
-
-func (sc *spanCache) Start() error {
-	sc.cache = make(map[string]*types.Trace)
-	return nil
-}
-
-func (sc *spanCache) Stop() error {
-	sc.cache = nil
-	return nil
-}
-
-func (sc *spanCache) Set(sp *types.Span) error {
-	traceID := sp.TraceID
-	trace, ok := sc.cache[traceID]
-	if !ok {
-		trace = &types.Trace{
-			APIHost:     sp.APIHost,
-			APIKey:      sp.APIKey,
-			Dataset:     sp.Dataset,
-			TraceID:     traceID,
-			ArrivalTime: sc.Clock.Now(),
-		}
-		sc.cache[traceID] = trace
-	}
-	trace.AddSpan(sp)
-	return nil
-}
-
-func (sc *spanCache) Get(traceID string) (*types.Trace, error) {
-	trace, ok := sc.cache[traceID]
-	if !ok {
-		return nil, nil
-	}
-	return trace, nil
-}
-
-func (sc *spanCache) GetOldest(n int) []*types.Trace {
-	// this is a naive implementation that just returns the first n traces
-	// in the cache. in a real implementation, we would want to sort the
-	// traces by arrival time and return the oldest n.
-	// It's useful for benchmarks because it's probably the fastest way to
-	// get n traces.
-	traces := make([]*types.Trace, 0, n)
-	for _, trace := range sc.cache {
-		traces = append(traces, trace)
-		if len(traces) == n {
-			break
-		}
-	}
-	return traces
-}
-
-func (sc *spanCache) Remove(traceID string) error {
-	delete(sc.cache, traceID)
-	return nil
-}
-
-func (sc *spanCache) Len() int {
-	return len(sc.cache)
+	// Clock returns the clock used by the cache.
+	GetClock() clockwork.Clock
 }
---
+++ collect/cache/span_cache_complex.go
@@ -0,0 +1,161 @@
+package cache
+
+import (
+	"sort"
+	"sync"
+
+	"github.com/facebookgo/startstop"
+	"github.com/honeycombio/refinery/config"
+	"github.com/honeycombio/refinery/types"
+	"github.com/jonboulle/clockwork"
+)
+
+// this cache is designed to be performant by pooling the allocated objects and
+// reusing them.
+// List of spans have capacity
+type spanCache_complex struct {
+	Cfg       config.Config   `inject:""`
+	Clock     clockwork.Clock `inject:""`
+	active    map[string]int
+	freeSlots []int
+	cache     []*types.Trace
+	current   []string
+	nextix    int
+	mut       sync.RWMutex
+}
+
+// ensure that spanCache implements SpanCache
+var _ SpanCache = &spanCache_complex{}
+
+// ensure that spanCache implements startstop.Starter
+var _ startstop.Starter = &spanCache_complex{}
+
+// ensure that spanCache implements startstop.Stopper
+var _ startstop.Stopper = &spanCache_complex{}
+
+func (sc *spanCache_complex) Start() error {
+	cfg, err := sc.Cfg.GetCollectionConfig()
+	if err != nil {
+		return err
+	}
+	sc.active = make(map[string]int, cfg.CacheCapacity)
+	sc.freeSlots = make([]int, 0, cfg.CacheCapacity)
+	sc.cache = make([]*types.Trace, 0, cfg.CacheCapacity)
+	return nil
+}
+
+func (sc *spanCache_complex) Stop() error {
+	sc.cache = nil
+	return nil
+}
+
+func (sc *spanCache_complex) GetClock() clockwork.Clock {
+	return sc.Clock
+}
+
+func (sc *spanCache_complex) Set(sp *types.Span) error {
+	var trace *types.Trace
+	traceID := sp.TraceID
+	sc.mut.Lock()
+	defer sc.mut.Unlock()
+	index, ok := sc.active[traceID]
+	if ok {
+		trace = sc.cache[index]
+	} else {
+		// we don't have a trace for this span yet so we need to set one up
+		// see if we have any free slots
+		if len(sc.freeSlots) > 0 {
+			// we have a free slot, so we can reuse it
+			// pop the last element off the freeSlots slice
+			index = sc.freeSlots[len(sc.freeSlots)-1]
+			sc.freeSlots = sc.freeSlots[:len(sc.freeSlots)-1]
+			trace = sc.cache[index]
+		} else {
+			// we don't have any free slots, so we need to allocate a new slot
+			// on the end of the cache
+			index = len(sc.cache)
+			trace = &types.Trace{}
+			sc.cache = append(sc.cache, trace)
+		}
+		sc.active[traceID] = index
+	}
+	trace.APIHost = sp.APIHost
+	trace.APIKey = sp.APIKey
+	trace.Dataset = sp.Dataset
+	trace.TraceID = traceID
+	trace.ArrivalTime = sc.Clock.Now()
+	trace.AddSpan(sp)
+	return nil
+}
+
+func (sc *spanCache_complex) Get(traceID string) *types.Trace {
+	sc.mut.RLock()
+	defer sc.mut.RUnlock()
+	index, ok := sc.active[traceID]
+	if !ok {
+		return nil
+	}
+	return sc.cache[index]
+}
+
+// Returns the oldest fraction of traces in the cache. This is used to decide
+// which traces to drop when the cache is full. It's moderately expensive
+// because it has to sort the traces by arrival time, but I couldn't find a
+// faster way to do it.
+func (sc *spanCache_complex) GetOldest(fract float64) []string {
+	n := int(float64(len(sc.active)) * fract)
+	ret := make([]string, 0, n)
+	sc.mut.RLock()
+	for traceID := range sc.active {
+		ret = append(ret, traceID)
+	}
+	sc.mut.RUnlock()
+	sort.Slice(ret, func(i, j int) bool {
+		t1 := sc.cache[sc.active[ret[i]]]
+		t2 := sc.cache[sc.active[ret[j]]]
+		return t1.ArrivalTime.Before(t2.ArrivalTime)
+	})
+	if len(ret) < n {
+		n = len(ret)
+	}
+	return ret[:n]
+}
+
+// This gets a batch of up to n traceIDs from the cache; it's used to get a
+// batch of traceIDs to process in parallel. It snapshots the active map and
+// returns a slice of traceIDs that were current at the time of the call. It
+// will return successive slices of traceIDs until it has returned all of them,
+// then it will start over from a fresh snapshot.
+func (sc *spanCache_complex) GetTraceIDs(n int) []string {
+	// this is the only function that looks at current or nextix so it
+	// doesn't need to lock those fields
+	if sc.current == nil || sc.nextix >= len(sc.current) {
+		sc.mut.RLock()
+		sc.current = make([]string, 0, len(sc.active))
+		for traceID := range sc.active {
+			sc.current = append(sc.current, traceID)
+		}
+		sc.mut.RUnlock()
+		sc.nextix = 0
+	}
+	if sc.nextix+n > len(sc.current) {
+		n = len(sc.current) - sc.nextix
+	}
+	return sc.current[sc.nextix : sc.nextix+n]
+}
+
+func (sc *spanCache_complex) Remove(traceID string) {
+	sc.mut.Lock()
+	defer sc.mut.Unlock()
+	index, ok := sc.active[traceID]
+	if !ok {
+		return
+	}
+	// we don't have to touch the cache itself, just the active map and the freeSlots
+	delete(sc.active, traceID)
+	sc.freeSlots = append(sc.freeSlots, index)
+}
+
+func (sc *spanCache_complex) Len() int {
+	return len(sc.active)
+}
