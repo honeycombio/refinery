33bb5e3b25d0cc3e296e280ea29f1b6897ba1765
Mike Goldsmth <goldsmith.mike@gmail.com>
2024-05-23 15:44:28 +0100

    add otlp logs tests

---
+++ route/otlp_logs_test.go
@@ -0,0 +1,329 @@
+package route
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	huskyotlp "github.com/honeycombio/husky/otlp"
+	"github.com/honeycombio/refinery/collect"
+	"github.com/honeycombio/refinery/collect/stressRelief"
+	"github.com/honeycombio/refinery/config"
+	"github.com/honeycombio/refinery/logger"
+	"github.com/honeycombio/refinery/metrics"
+	"github.com/honeycombio/refinery/transmit"
+	"github.com/klauspost/compress/zstd"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	collectorlogs "go.opentelemetry.io/proto/otlp/collector/logs/v1"
+	common "go.opentelemetry.io/proto/otlp/common/v1"
+	logs "go.opentelemetry.io/proto/otlp/logs/v1"
+	resource "go.opentelemetry.io/proto/otlp/resource/v1"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+)
+
+func TestLogsOTLPHandler(t *testing.T) {
+	md := metadata.New(map[string]string{"x-honeycomb-team": legacyAPIKey, "x-honeycomb-dataset": "ds"})
+	ctx := metadata.NewIncomingContext(context.Background(), md)
+
+	mockMetrics := metrics.MockMetrics{}
+	mockMetrics.Start()
+	mockTransmission := &transmit.MockTransmission{}
+	mockTransmission.Start()
+	decoders, err := makeDecoders(1)
+	if err != nil {
+		t.Error(err)
+	}
+	router := &Router{
+		Config: &config.MockConfig{
+			TraceIdFieldNames: []string{"trace.trace_id"},
+		},
+		Collector: &collect.CentralCollector{
+			Transmission: mockTransmission,
+			StressRelief: &stressRelief.MockStressReliever{
+				IsStressed: true, // mimic the collector being stressed so we send events immediately
+			},
+		},
+		Metrics:              &mockMetrics,
+		UpstreamTransmission: mockTransmission,
+		iopLogger: iopLogger{
+			Logger:         &logger.MockLogger{},
+			incomingOrPeer: "incoming",
+		},
+		Logger:           &logger.MockLogger{},
+		zstdDecoders:     decoders,
+		environmentCache: newEnvironmentCache(time.Second, nil),
+	}
+
+	t.Run("can receive OTLP over gRPC", func(t *testing.T) {
+		req := &collectorlogs.ExportLogsServiceRequest{
+			ResourceLogs: []*logs.ResourceLogs{{
+				ScopeLogs: []*logs.ScopeLogs{{
+					LogRecords: createLogsRecords(),
+				}},
+			}},
+		}
+		logsServer := NewLogsServer(router)
+		_, err := logsServer.Export(ctx, req)
+		if err != nil {
+			t.Errorf(`Unexpected error: %s`, err)
+		}
+		assert.Equal(t, 1, len(mockTransmission.Events))
+		mockTransmission.Flush()
+	})
+
+	t.Run("invalid headers", func(t *testing.T) {
+		req := &collectorlogs.ExportLogsServiceRequest{}
+		body, err := proto.Marshal(req)
+		assert.NoError(t, err)
+		anEmptyRequestBody := bytes.NewReader(body) // Empty because we're testing headers, not the body.
+
+		testCases := []struct {
+			name                        string
+			requestContentType          string
+			expectedResponseStatus      int
+			expectedResponseContentType string
+			expectedResponseBody        string
+		}{
+			{
+				name:                        "no key/bad content-type",
+				requestContentType:          "application/nope",
+				expectedResponseStatus:      http.StatusUnsupportedMediaType, // Prioritize erroring on bad content type over other header issues.
+				expectedResponseContentType: "text/plain",
+				expectedResponseBody:        huskyotlp.ErrInvalidContentType.Message,
+			},
+			{
+				name:                        "no key/json",
+				requestContentType:          "application/json",
+				expectedResponseStatus:      http.StatusUnauthorized,
+				expectedResponseContentType: "application/json",
+				expectedResponseBody:        fmt.Sprintf("{\"message\":\"%s\"}", huskyotlp.ErrMissingAPIKeyHeader.Message),
+			},
+			{
+				name:                        "no key/protobuf",
+				requestContentType:          "application/protobuf",
+				expectedResponseStatus:      http.StatusUnauthorized,
+				expectedResponseContentType: "application/protobuf",
+				expectedResponseBody:        fmt.Sprintf("\x12!%s", huskyotlp.ErrMissingAPIKeyHeader.Message),
+			},
+		}
+
+		for _, tC := range testCases {
+			t.Run(tC.name, func(t *testing.T) {
+				request, err := http.NewRequest("POST", "/v1/traces", anEmptyRequestBody)
+				require.NoError(t, err)
+				request.Header = http.Header{}
+				request.Header.Set("content-type", tC.requestContentType)
+				response := httptest.NewRecorder()
+				router.postOTLPTrace(response, request)
+
+				assert.Equal(t, tC.expectedResponseStatus, response.Code)
+				assert.Equal(t, tC.expectedResponseContentType, response.Header().Get("content-type"))
+				assert.Equal(t, tC.expectedResponseBody, response.Body.String())
+			})
+		}
+	})
+
+	t.Run("can receive OTLP over HTTP/protobuf", func(t *testing.T) {
+		req := &collectorlogs.ExportLogsServiceRequest{
+			ResourceLogs: []*logs.ResourceLogs{{
+				ScopeLogs: []*logs.ScopeLogs{{
+					LogRecords: createLogsRecords(),
+				}},
+			}},
+		}
+		body, err := proto.Marshal(req)
+		if err != nil {
+			t.Error(err)
+		}
+
+		request, _ := http.NewRequest("POST", "/v1/logs", strings.NewReader(string(body)))
+		request.Header = http.Header{}
+		request.Header.Set("content-type", "application/protobuf")
+		request.Header.Set("x-honeycomb-team", legacyAPIKey)
+		request.Header.Set("x-honeycomb-dataset", "dataset")
+
+		w := httptest.NewRecorder()
+		router.postOTLPLogs(w, request)
+		assert.Equal(t, w.Code, http.StatusOK)
+
+		assert.Equal(t, 1, len(mockTransmission.Events))
+		mockTransmission.Flush()
+	})
+
+	t.Run("can receive OTLP over HTTP/protobuf with gzip encoding", func(t *testing.T) {
+		req := &collectorlogs.ExportLogsServiceRequest{
+			ResourceLogs: []*logs.ResourceLogs{{
+				ScopeLogs: []*logs.ScopeLogs{{
+					LogRecords: createLogsRecords(),
+				}},
+			}},
+		}
+		body, err := proto.Marshal(req)
+		if err != nil {
+			t.Error(err)
+		}
+
+		buf := new(bytes.Buffer)
+		writer := gzip.NewWriter(buf)
+		writer.Write(body)
+		writer.Close()
+		if err != nil {
+			t.Error(err)
+		}
+
+		request, _ := http.NewRequest("POST", "/v1/logs", strings.NewReader(buf.String()))
+		request.Header = http.Header{}
+		request.Header.Set("content-type", "application/protobuf")
+		request.Header.Set("content-encoding", "gzip")
+		request.Header.Set("x-honeycomb-team", legacyAPIKey)
+		request.Header.Set("x-honeycomb-dataset", "dataset")
+
+		w := httptest.NewRecorder()
+		router.postOTLPLogs(w, request)
+		assert.Equal(t, w.Code, http.StatusOK)
+
+		assert.Equal(t, 1, len(mockTransmission.Events))
+		mockTransmission.Flush()
+	})
+
+	t.Run("can receive OTLP over HTTP/protobuf with zstd encoding", func(t *testing.T) {
+		req := &collectorlogs.ExportLogsServiceRequest{
+			ResourceLogs: []*logs.ResourceLogs{{
+				ScopeLogs: []*logs.ScopeLogs{{
+					LogRecords: createLogsRecords(),
+				}},
+			}},
+		}
+		body, err := proto.Marshal(req)
+		if err != nil {
+			t.Error(err)
+		}
+
+		buf := new(bytes.Buffer)
+		writer, err := zstd.NewWriter(buf)
+		if err != nil {
+			t.Error(err)
+		}
+		writer.Write(body)
+		writer.Close()
+		if err != nil {
+			t.Error(err)
+		}
+
+		request, _ := http.NewRequest("POST", "/v1/logs", strings.NewReader(buf.String()))
+		request.Header = http.Header{}
+		request.Header.Set("content-type", "application/protobuf")
+		request.Header.Set("content-encoding", "zstd")
+		request.Header.Set("x-honeycomb-team", legacyAPIKey)
+		request.Header.Set("x-honeycomb-dataset", "dataset")
+
+		w := httptest.NewRecorder()
+		router.postOTLPLogs(w, request)
+		assert.Equal(t, w.Code, http.StatusOK)
+
+		assert.Equal(t, 1, len(mockTransmission.Events))
+		mockTransmission.Flush()
+	})
+
+	t.Run("accepts OTLP over HTTP/JSON ", func(t *testing.T) {
+		req := &collectorlogs.ExportLogsServiceRequest{
+			ResourceLogs: []*logs.ResourceLogs{{
+				ScopeLogs: []*logs.ScopeLogs{{
+					LogRecords: createLogsRecords(),
+				}},
+			}},
+		}
+		body, err := protojson.Marshal(req)
+		if err != nil {
+			t.Error(err)
+		}
+
+		request, _ := http.NewRequest("POST", "/v1/logs", bytes.NewReader(body))
+		request.Header = http.Header{}
+		request.Header.Set("content-type", "application/json")
+		request.Header.Set("x-honeycomb-team", legacyAPIKey)
+		request.Header.Set("x-honeycomb-dataset", "dataset")
+
+		w := httptest.NewRecorder()
+		router.postOTLPLogs(w, request)
+		assert.Equal(t, w.Code, http.StatusOK)
+		assert.Equal(t, "{}", w.Body.String())
+
+		assert.Equal(t, 1, len(mockTransmission.Events))
+		mockTransmission.Flush()
+	})
+
+	t.Run("rejects bad API keys", func(t *testing.T) {
+		router.Config.(*config.MockConfig).IsAPIKeyValidFunc = func(k string) bool { return false }
+		req := &collectorlogs.ExportLogsServiceRequest{
+			ResourceLogs: []*logs.ResourceLogs{{
+				ScopeLogs: []*logs.ScopeLogs{{
+					LogRecords: createLogsRecords(),
+				}},
+			}},
+		}
+		body, err := protojson.Marshal(req)
+		if err != nil {
+			t.Error(err)
+		}
+
+		request, _ := http.NewRequest("POST", "/v1/logs", bytes.NewReader(body))
+		request.Header = http.Header{}
+		request.Header.Set("content-type", "application/json")
+		request.Header.Set("x-honeycomb-team", legacyAPIKey)
+		request.Header.Set("x-honeycomb-dataset", "dataset")
+
+		w := httptest.NewRecorder()
+		router.postOTLPLogs(w, request)
+		assert.Equal(t, http.StatusUnauthorized, w.Code)
+		assert.Contains(t, w.Body.String(), "not found in list of authorized keys")
+
+		assert.Equal(t, 0, len(mockTransmission.Events))
+		mockTransmission.Flush()
+	})
+}
+
+func createLogsRecords() []*logs.LogRecord {
+	now := time.Now()
+	// generating the test OTLP trace data
+	// traceId := make([]byte, 16)
+	// spanId := make([]byte, 8)
+	// rand.Read(traceId)
+	// rand.Read(spanId)
+	return []*logs.LogRecord{
+		{
+			TimeUnixNano: uint64(now.UnixNano()),
+			Body: &common.AnyValue{
+				Value: &common.AnyValue_StringValue{StringValue: "log message"},
+			},
+			Attributes: []*common.KeyValue{
+				{
+					Key: "attribute_key",
+					Value: &common.AnyValue{
+						Value: &common.AnyValue_StringValue{StringValue: "attribute_value"},
+					},
+				},
+			},
+			SeverityText: "INFO",
+			// TraceId:      traceId,
+			// SpanId:       spanId,
+		},
+	}
+}
+
+func createResource() *resource.Resource {
+	return &resource.Resource{
+		Attributes: []*common.KeyValue{
+			{Key: "service.name", Value: &common.AnyValue{Value: &common.AnyValue_StringValue{StringValue: "my-service"}}},
+		},
+	}
+}
