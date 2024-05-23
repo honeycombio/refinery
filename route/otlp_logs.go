820d6fa230c4580645417b09d899acd84f7c5d7c
Mike Goldsmth <goldsmith.mike@gmail.com>
2024-05-23 14:33:56 +0100

    make logs handler mirror traces, move shared to common file

--- route/otlp_logs.go
+++ route/otlp_logs.go
@@ -7,36 +7,39 @@ import (
 	"net/http"
 
 	huskyotlp "github.com/honeycombio/husky/otlp"
+
 	collectorlogs "go.opentelemetry.io/proto/otlp/collector/logs/v1"
 )
 
 func (r *Router) postOTLPLogs(w http.ResponseWriter, req *http.Request) {
 	ri := huskyotlp.GetRequestInfoFromHttpHeaders(req.Header)
 
-	if !r.Config.IsAPIKeyValid(ri.ApiKey) {
-		err := fmt.Errorf("api key %s not found in list of authorized keys", ri.ApiKey)
-		r.handlerReturnWithError(w, ErrAuthNeeded, err)
-		return
-	}
-
 	if err := ri.ValidateLogsHeaders(); err != nil {
 		if errors.Is(err, huskyotlp.ErrInvalidContentType) {
 			r.handlerReturnWithError(w, ErrInvalidContentType, err)
 		} else {
-			r.handlerReturnWithError(w, ErrAuthNeeded, err)
+			r.handleOTLPFailureResponse(w, req, huskyotlp.OTLPError{Message: err.Error(), HTTPStatusCode: http.StatusUnauthorized})
 		}
+		return
+	}
+
+	if !r.Config.IsAPIKeyValid(ri.ApiKey) {
+		r.handleOTLPFailureResponse(w, req, huskyotlp.OTLPError{Message: fmt.Sprintf("api key %s not found in list of authorized keys", ri.ApiKey), HTTPStatusCode: http.StatusUnauthorized})
 		return
 	}
 
 	result, err := huskyotlp.TranslateLogsRequestFromReader(req.Context(), req.Body, ri)
 	if err != nil {
-		r.handlerReturnWithError(w, ErrUpstreamFailed, err)
+		r.handleOTLPFailureResponse(w, req, huskyotlp.OTLPError{Message: err.Error(), HTTPStatusCode: http.StatusInternalServerError})
 		return
 	}
 
 	if err := r.processOTLPRequest(req.Context(), result.Batches, ri.ApiKey); err != nil {
-		r.handlerReturnWithError(w, ErrUpstreamFailed, err)
+		r.handleOTLPFailureResponse(w, req, huskyotlp.OTLPError{Message: err.Error(), HTTPStatusCode: http.StatusInternalServerError})
+		return
 	}
+
+	_ = huskyotlp.WriteOtlpHttpTraceSuccessResponse(w, req)
 }
 
 type LogsServer struct {
