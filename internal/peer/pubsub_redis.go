72ac6b8f15428ec3a138133b7bbcf0d945a8db81
Yingrong Zhao <yingrongzhao@yingrongzhao.attlocal.net>
2024-08-01 16:12:17 -0400

    add comments

--- internal/peer/pubsub_redis.go
+++ internal/peer/pubsub_redis.go
@@ -77,7 +77,13 @@ type RedisPubsubPeers struct {
 	Logger  logger.Logger   `inject:""`
 	PubSub  pubsub.PubSub   `inject:""`
 	Clock   clockwork.Clock `inject:""`
-	Done    chan struct{}
+
+	// Done is a channel that will be closed when the service should stop.
+	// After it is closed, peers service should signal the rest of the cluster
+	// that it is no longer available.
+	// However, any messages send on the peers channel will still be processed
+	// since the pubsub subscription is still active.
+	Done chan struct{}
 
 	// Done is a channel that will be closed when the service should stop.
 	// After it is closed, peers service should signal the rest of the cluster
