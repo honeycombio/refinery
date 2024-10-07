e5fd6d86103df71fc930342cc9474aa6a76ebb9a
Kent Quirk <kentquirk@honeycomb.io>
2024-10-07 08:05:06 -0400

    fix: Put a limit on the size of sampler keys (#1364)
    ## Which problem is this PR solving?
    
    - If someone sends a big trace where one of the sampler keys is a
    high-cardinality field, Refinery could generate a huge sampler key
    value. This causes problems both for refinery but also the downstream
    telemetry. Let's not do that.
    
    ## Short description of the changes
    
    - Put a limit of 100 unique values to make up any one sampler key. Even
    that is big and probably useless but it should avoid any real use cases.
    - Add a test to show it works.
    
    Fixes #1363
    
    ---------
    
    Co-authored-by: Mike Goldsmth <goldsmith.mike@gmail.com>

--- sample/trace_key.go
+++ sample/trace_key.go
@@ -6,8 +6,15 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/honeycombio/refinery/generics"
 	"github.com/honeycombio/refinery/types"
 )
+
+// once a key gets this many unique values, it's off the charts in terms of uniqueness
+// so we just stop looking for more.
+// This is a safety valve to prevent us from someone sending a high-cardinality field
+// in a giant trace.
+const maxKeyLength = 100
 
 type traceKey struct {
 	fields         []string
@@ -37,49 +44,66 @@ func newTraceKey(fields []string, useTraceLength bool) *traceKey {
 }
 
 // build, builds the trace key based on the configuration of the traceKeyGenerator
-func (d *traceKey) build(trace *types.Trace) string {
+// returns the number of values used to build the key
+func (d *traceKey) build(trace *types.Trace) (string, int) {
+	fieldCount := 0
 	// fieldCollector gets all values from the fields listed in the config, even
 	// if they happen multiple times.
 	fieldCollector := make(map[string][]string)
 
 	// for each field, for each span, get the value of that field
 	spans := trace.GetSpans()
+	uniques := generics.NewSetWithCapacity[string](maxKeyLength)
+outer:
 	for _, field := range d.fields {
 		for _, span := range spans {
 			if val, ok := span.Data[field]; ok {
+				u := fmt.Sprintf("%s/%v", field, val)
+				// don't bother to add it if we've already seen it
+				if uniques.Contains(u) {
+					continue
+				}
+				uniques.Add(u)
+				if len(uniques) >= maxKeyLength {
+					break outer
+				}
 				fieldCollector[field] = append(fieldCollector[field], fmt.Sprintf("%v", val))
 			}
 		}
 	}
-	// ok, now we have a map of fields to a list of all values for that field.
+	// ok, now we have a map of fields to a list of all unique values for that field.
+	// (unless it was huge, in which case we have a bunch of them)
 
-	var key string
+	var key strings.Builder
 	for _, field := range d.fields {
 		// sort and collapse list
 		sort.Strings(fieldCollector[field])
 		var prevStr string
 		for _, str := range fieldCollector[field] {
 			if str != prevStr {
-				key += str + "•"
+				key.WriteString(str)
+				key.WriteRune('•')
+				fieldCount += 1
 			}
 			prevStr = str
 		}
 		// get ready for the next element
-		key += ","
+		key.WriteRune(',')
 	}
 
 	if trace.RootSpan != nil {
 		for _, field := range d.rootOnlyFields {
-
 			if val, ok := trace.RootSpan.Data[field]; ok {
-				key += fmt.Sprintf("%v,", val)
+				key.WriteString(fmt.Sprintf("%v,", val))
+				fieldCount += 1
 			}
 		}
 	}
 
 	if d.useTraceLength {
-		key += strconv.FormatInt(int64(len(spans)), 10)
+		key.WriteString(strconv.FormatInt(int64(len(spans)), 10))
+		fieldCount += 1
 	}
 
-	return key
+	return key.String(), fieldCount
 }
--- sample/windowed_throughput.go
+++ sample/windowed_throughput.go
@@ -76,7 +76,10 @@ func (d *WindowedThroughputSampler) SetClusterSize(size int) {
 }
 
 func (d *WindowedThroughputSampler) GetSampleRate(trace *types.Trace) (rate uint, keep bool, reason string, key string) {
-	key = d.key.build(trace)
+	key, n := d.key.build(trace)
+	if n == maxKeyLength {
+		d.Logger.Debug().Logf("trace key hit max length of %d, truncating", maxKeyLength)
+	}
 	count := int(trace.DescendantCount())
 	rate = uint(d.dynsampler.GetSampleRateMulti(key, count))
 	if rate < 1 { // protect against dynsampler being broken even though it shouldn't be
