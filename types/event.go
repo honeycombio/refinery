d1411849b938a06e8bae03f715a4b9deaf539443
Yingrong Zhao <22300958+VinozzZ@users.noreply.github.com>
2025-08-18 11:05:41 -0400

    maint: clean up unused code (#1671)

--- types/event.go
+++ types/event.go
@@ -200,11 +200,6 @@ func (t *Trace) SpanEventCount() uint32 {
 	return t.spanEventCount
 }
 
-// IsOrphan returns true if the trace is older than 4 times the traceTimeout
-func (t *Trace) IsOrphan(traceTimeout time.Duration, now time.Time) bool {
-	return now.Sub(t.SendBy) >= traceTimeout*4
-}
-
 // Span is an event that shows up with a trace ID, so will be part of a Trace
 // This is not thread-safe; only one goroutine should be working with a span object at a time.
 type Span struct {
