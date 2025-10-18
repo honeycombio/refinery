75dbdd8fa12f6a317c0245df102d609924f0a672
Yingrong Zhao <22300958+VinozzZ@users.noreply.github.com>
2025-10-18 09:44:30 -0400

    maint: consolidate Peer mocks into one mock implementation (#1717)

--- internal/peer/mock.go
+++ internal/peer/mock.go
@@ -1,31 +1,70 @@
 package peer
 
+import (
+	"sync"
+)
+
 var _ Peers = (*MockPeers)(nil)
 
 type MockPeers struct {
-	Peers []string
-	ID    string
+	mut       sync.RWMutex
+	peers     []string
+	id        string
+	callbacks []func()
+}
+
+func NewMockPeers(peers []string, id string) *MockPeers {
+	return &MockPeers{
+		peers: peers,
+		id:    id,
+	}
 }
 
 func (p *MockPeers) GetPeers() ([]string, error) {
-	return p.Peers, nil
+	p.mut.RLock()
+	defer p.mut.RUnlock()
+	return p.peers, nil
 }
 
 func (p *MockPeers) GetInstanceID() (string, error) {
-	return p.ID, nil
+	p.mut.RLock()
+	defer p.mut.RUnlock()
+
+	return p.id, nil
 }
 
 func (p *MockPeers) RegisterUpdatedPeersCallback(callback func()) {
-	callback()
+	p.mut.Lock()
+	defer p.mut.Unlock()
+	p.callbacks = append(p.callbacks, callback)
 }
 
 func (p *MockPeers) Start() error {
-	if len(p.ID) == 0 && len(p.Peers) > 0 {
-		p.ID = p.Peers[0]
+	p.mut.Lock()
+	defer p.mut.Unlock()
+	if len(p.id) == 0 && len(p.peers) > 0 {
+		p.id = p.peers[0]
 	}
 	return nil
 }
 
 func (p *MockPeers) Ready() error {
 	return nil
+}
+
+// UpdatePeers changes the peer list and triggers callbacks
+func (p *MockPeers) UpdatePeers(newPeers []string) {
+	p.mut.Lock()
+	p.peers = newPeers
+	if len(newPeers) > 0 && p.id == "" {
+		p.id = newPeers[0]
+	}
+	callbacks := make([]func(), len(p.callbacks))
+	copy(callbacks, p.callbacks)
+	p.mut.Unlock()
+
+	// Trigger callbacks outside the lock
+	for _, callback := range callbacks {
+		callback()
+	}
 }
