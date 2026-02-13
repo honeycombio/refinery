82797f7f40106cbae43d33185b58dca8144995b1
Mike Terhar <mike@terhar.com>
2026-02-13 15:21:12 +0000

    Reduce PR by putting headers back in types

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -2,17 +2,6 @@ package headers
 
 import "strings"
 
-// Honeycomb API header constants
-const (
-	APIKeyHeader = "X-Honeycomb-Team"
-	// libhoney-js uses this
-	APIKeyHeaderShort = "X-Hny-Team"
-	DatasetHeader     = "X-Honeycomb-Dataset"
-	SampleRateHeader  = "X-Honeycomb-Samplerate"
-	TimestampHeader   = "X-Honeycomb-Event-Time"
-	QueryTokenHeader  = "X-Honeycomb-Refinery-Query"
-)
-
 // reservedPrefixes contains HTTP header prefixes that cannot be overridden
 // via AdditionalHeaders because they are reserved for Honeycomb API communication.
 var reservedPrefixes = []string{
